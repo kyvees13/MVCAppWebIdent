0cc1ab65e8d28cb2202d5ba50fb3907e81c1e9e2
qvs <kyvees13@gmail.com>
2022-06-15 10:35:29 +0500

    Добавьте файлы проекта.

---
+++ MVCAppWebIdent/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCAppWebIdent.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize(Policy = "Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "Admin")]
+        public IActionResult Roles()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVCAppWebIdent/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MVCAppWebIdent.Models;
+using System.Diagnostics;
+
+namespace MVCAppWebIdent.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Strategies()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "Administrator")]
+        public IActionResult Roles()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
