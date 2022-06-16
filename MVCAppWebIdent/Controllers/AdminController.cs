e1c2ad17da105a2609f52e57536b6685ccae450a
qvs <kyvees13@gmail.com>
2022-06-16 16:11:29 +0500

    za

--- MVCAppWebIdent/Controllers/AdminController.cs
+++ MVCAppWebIdent/Controllers/AdminController.cs
@@ -6,10 +6,12 @@ namespace MVCAppWebIdent.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        public AdminController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
+            _userManager = userManager;
             _roleManager = roleManager;
         }
 
@@ -22,7 +24,22 @@ namespace MVCAppWebIdent.Controllers
         [Authorize(Policy = "Admin")]
         public IActionResult Roles()
         {
-            return View();
+            return View(_userManager.Users);
+        }
+
+        [Authorize(Policy = "Admin")]
+        public IActionResult Edit(string id)
+        {
+            var a = 0;
+            return Ok(a);
+        }
+
+        [Authorize(Policy = "Admin")]
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            var a = 0;
+            return Ok(a);
         }
 
     }
