300662933912c0506dcd0fe500878aa4ca18d612
qvs <kyvees13@gmail.com>
2022-06-24 02:59:43 +0500

    фывфыв

--- MVCAppWebIdent/Controllers/AdminController.cs
+++ MVCAppWebIdent/Controllers/AdminController.cs
@@ -30,13 +30,16 @@ namespace MVCAppWebIdent.Controllers
         [Authorize(Policy = "Admin")]
         public IActionResult GetPopUpDeleteModalPartialView() => PartialView("_PopupDeleteRoleModelPartial");
 
+        [Authorize(Policy = "Admin")]
+        public IActionResult GetPopUpDetailsModalPartialView() => PartialView("_PopupDetailsRoleModelPartial");
+
         [Authorize(Policy ="Admin")]
         [HttpPost]
-        public JsonResult SetNewRole(string roleName)
+        public JsonResult AddRole(string roleName)
         {
             var role = _roleManager.FindByNameAsync(roleName);
 
-            if (role == null)
+            if (role.Result == null)
             {
                 var result = _roleManager.CreateAsync(new IdentityRole(roleName));
                 
@@ -53,6 +56,95 @@ namespace MVCAppWebIdent.Controllers
             {
                 return Json(new { Success = "False", Message = "Role is exist" });
             }
+        }
+
+        [Authorize(Policy = "Admin")]
+        [HttpPost]
+        public JsonResult DeleteRole(string id)
+        {
+            var role = _roleManager.FindByIdAsync(id).Result;
+
+            if (role != null)
+            {
+                var result = _roleManager.DeleteAsync(role).Result;
+
+                if (result.Succeeded)
+                {
+                    return Json(new { Success = "True", Message = "Role was deleted" });
+                }
+                else
+                {
+                    return Json(new { Success = "False", Message = "Role was not deleted" });
+                }
+            }
+            else
+            {
+                return Json(new { Success = "False", Message = "Role is not exist" });
+            }
+        }
+
+        [Authorize(Policy = "Admin")]
+        [HttpPost]
+        public JsonResult SetRole(string userid, string roleName)
+        {
+            var role = _roleManager.FindByNameAsync(roleName).Result;
+
+            if (role != null)
+            {
+                var user = _userManager.FindByIdAsync(userid).Result;
+
+                if (user == null)
+                {
+                    return Json(new { Success = "False", Message = "User not found" });
+                }
+                else
+                {
+                    if (!_userManager.IsInRoleAsync(user, roleName).Result)
+                    {
+                        return Json(new { Success = "True", Message = "Success set role" });
+                    }
+                    else
+                    {
+                        return Json(new { Success = "False", Message = "User has already in role" });
+                    }
+                }
+            }
+            else
+            {
+                return Json(new { Success = "False", Message = "Role is not exist" });
+            }
+        }
+
+        [Authorize(Policy = "Admin")]
+        [HttpGet]
+        public JsonResult GetRoles()
+        {
+            var _list = new List<string>();
+
+            foreach (var role in _roleManager.Roles.ToList())
+            {
+                _list.Append(role.Name);
+            }
+
+            return Json(new { Roles = JsonSerializer.Serialize(_list) });
+        }
+
+        [Authorize(Policy = "Admin")]
+        public IActionResult _SelectPartialView()
+        {
+            List<SelectListItem> _select = new List<SelectListItem>();
+
+            var roles = _roleManager.Roles;
+
+            if (roles != null)
+            {
+                foreach (IdentityRole role in roles)
+                    _select.Add(new SelectListItem { Text = role.Name });
+            }
+
+            ViewBag.select = _select;
+
+            return PartialView("_SelectPartialView");
         }
 
         [Authorize(Policy = "Admin")]
@@ -94,6 +186,7 @@ namespace MVCAppWebIdent.Controllers
                 return Json(new { Success = "False", Message = "User not found" });
             }
 
+            currentUser.UserName = _EditModel.Username;
             currentUser.Email = _EditModel.Email;
             currentUser.PhoneNumber = _EditModel.Phonenumber;
 
@@ -119,11 +212,11 @@ namespace MVCAppWebIdent.Controllers
                 return Json(new { Success = "False", Message = "User not found" });
             }
 
-            var removerolesResponse = _userManager.RemoveFromRolesAsync(user, _userManager.GetRolesAsync(user).Result.Cast<string>());
+            // var removerolesResponse = _userManager.RemoveFromRolesAsync(user, _userManager.GetRolesAsync(user).Result.Cast<string>());
 
-            var deleteuserResponse = _userManager.DeleteAsync(user);
+            var deleteuserResponse = _userManager.DeleteAsync(user).Result;
 
-            if (deleteuserResponse.Result.Succeeded)
+            if (deleteuserResponse.Succeeded)
             {
                 return Json(new
                 {
@@ -131,24 +224,27 @@ namespace MVCAppWebIdent.Controllers
                     Message = "Success deleted"
                 });
             }
-            else if (deleteuserResponse.Result.Errors.Count() > 0)
+            else if (deleteuserResponse.Errors.Count() > 0)
             {
                 return Json(new
                 {
                     Success = "False",
-                    Message = JsonSerializer.Serialize(deleteuserResponse.Result.Errors.ToList())
+                    Message = JsonSerializer.Serialize(deleteuserResponse.Errors.ToList())
+                });
+            }
+            else
+            {
+                return Json(new
+                {
+                    Success = "False",
+                    Message = "Unexpected"
                 });
             }
 
-            return Json(new
-            {
-                Success = "False",
-                Message = "Unexpected"
-            });
         }
 
         [Authorize(Policy = "Admin")]
-        public IActionResult Roles()
+        public IActionResult Clients()
         {
             List<SelectListItem> _select = new List<SelectListItem>();
 
@@ -162,14 +258,17 @@ namespace MVCAppWebIdent.Controllers
 
             ViewBag.select = _select;
 
-            return View();
+            return PartialView();
         }
 
         [Authorize(Policy = "Admin")]
-        public IActionResult Settings()
-        {
-            return View();
-        }
+        public IActionResult Roles() => PartialView();
+
+        [Authorize(Policy = "Admin")]
+        public IActionResult Settings() => PartialView();
+
+        [Authorize(Policy = "Admin")]
+        public IActionResult GetRolesTablePartialView() => PartialView(_roleManager.Roles);
 
         [Authorize(Policy = "Admin")]
         public IActionResult GetTablePartialView(string roleName)
